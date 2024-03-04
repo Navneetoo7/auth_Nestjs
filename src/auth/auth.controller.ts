5041edabc4732afb8133af587a964665ee2f550f
navneet <mani@indovations.in>
2024-03-04 21:44:04 +0400

    security changes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,17 @@
-import { Body, Controller, HttpException, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthPayloadDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
+import { LoaclGuard } from './guards/local.guards';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
   @Post('login')
+  @UseGuards(LoaclGuard)
   login(@Body() authPayload: AuthPayloadDto) {
     const user = this.authService.validateUser(authPayload);
-    if (!user) throw new HttpException('Invalid credentials', 401);
-    return user
+    // if (!user) throw new HttpException('Invalid credentials', 401);
+    return user;
   }
 }
